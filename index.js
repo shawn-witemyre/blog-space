a5d04c13f2f89bfbb938e0ec4df8b7414a400478
Shawn Witemyre <switemyre@comcast.net>
2025-05-02 17:33:03 -0400

    added functionality to post posts refactored code by creating renderPost function

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+let postsArray = [];
+const titleInput = document.getElementById("post-title");
+const bodyInput = document.getElementById("post-body");
+const form = document.getElementById("new-post");
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
+            <h3>${post.title}</h3>
+            <p>${post.body}</p>
+            <hr />
+        `;
+  }
+  document.getElementById("blog-list").innerHTML = html;
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((response) => response.json())
+  .then((res) => res.json())
   .then((data) => {
-    const postsArr = data.slice(0, 5);
-    console.log(postsArr);
-    let htmlString = "";
-    postsArr.forEach(function (post) {
-      htmlString += `<h3>${post.title}</h3><p>${post.body}</p><hr />`;
-    });
-    document.getElementById("blog-list").innerHTML = htmlString;
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
-document.getElementById("new-post").addEventListener("submit", function (e) {
-  e.preventDefault();
-  const postTitle = document.getElementById("post-title").value;
-  const postBody = document.getElementById("post-body").value;
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const postTitle = titleInput.value;
+  const postBody = bodyInput.value;
   const data = {
     title: postTitle,
     body: postBody,
   };
-  console.log(data);
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      postsArray.unshift(post);
+      renderPosts();
+      form.reset();
+    });
 });
